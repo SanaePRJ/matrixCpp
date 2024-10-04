6bcaa6e2eb0b05070b833760c04c5c0325260c17
SanaePRJ <sanaeprj@gmail.com>
2024-10-04 11:50:48 +0900

    modify README.md,create some headerfile.

---
+++ matrixCpp/matrixCalc.hpp
@@ -0,0 +1,8 @@
+// 加算,減算,アダマール積など計算を行います。
+#ifndef MATRIXCPP_MATRIXCALC_HPP
+#define MATRIXCPP_MATRIXCALC_HPP
+
+
+
+
+#endif
---
+++ matrixCpp/matrixDec.hpp
@@ -0,0 +1,8 @@
+// LU分解など行列分解を行います。逆行列も含む。
+#ifndef MATRIXCPP_MATRIXDEC_HPP
+#define MATRIXCPP_MATRIXDEC_HPP
+
+
+
+
+#endif
